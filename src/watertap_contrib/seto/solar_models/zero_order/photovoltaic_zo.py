94e0acf7c0edda5ca821d5ce8e765c0679395788
kurbansitterley <kurban.sitterley@nrel.gov>
2023-01-06 09:49:49 -0700

    black, clean up

--- src/watertap_contrib/seto/solar_models/zero_order/photovoltaic_zo.py
+++ src/watertap_contrib/seto/solar_models/zero_order/photovoltaic_zo.py
@@ -1,17 +1,5 @@
-###############################################################################
-# WaterTAP Copyright (c) 2021, The Regents of the University of California,
-# through Lawrence Berkeley National Laboratory, Oak Ridge National
-# Laboratory, National Renewable Energy Laboratory, and National Energy
-# Technology Laboratory (subject to receipt of any required approvals from
-# the U.S. Dept. of Energy). All rights reserved.
-#
-# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
-# information, respectively. These files are also available online at the URL
-# "https://github.com/watertap-org/watertap/"
-#
-###############################################################################
 """
-This module contains a zero-order representation of a solar energy
+This module contains a zero-order representation of a photovoltaic
 operation.
 """
 
@@ -24,7 +12,7 @@ __author__ = "Kurban Sitterley"
 
 
 @declare_process_block_class("PhotovoltaicZO")
-class SolarEnergyZOData(ZeroOrderBaseData):
+class PhotovoltaicZOData(ZeroOrderBaseData):
     """
     Zero-Order model for photovoltaic system.
     """
--- src/watertap_contrib/seto/unit_models/surrogate/lt_med_surrogate.py
+++ src/watertap_contrib/seto/unit_models/surrogate/lt_med_surrogate.py
@@ -242,7 +242,7 @@ class LTMEDData(UnitModelBlockData):
         )
 
         # Add ports
-        self.add_port(name="feed", block=self.feed_props)
+        self.add_inlet_port(name="feed", block=self.feed_props)
         self.add_port(name="distillate", block=self.distillate_props)
         self.add_port(name="brine", block=self.brine_props)
         self.add_port(name="steam", block=self.steam_props)
@@ -827,7 +827,9 @@ class LTMEDData(UnitModelBlockData):
         iscale.constraint_scaling_transform(self.eq_steam_mass_flow, sf)
 
         sf = iscale.get_scaling_factor(self.specific_thermal_energy_consumption)
-        iscale.constraint_scaling_transform(self.eq_specific_thermal_energy_consumption, sf)
+        iscale.constraint_scaling_transform(
+            self.eq_specific_thermal_energy_consumption, sf
+        )
 
         sf = iscale.get_scaling_factor(self.thermal_power_requirement)
         iscale.constraint_scaling_transform(self.eq_thermal_power_requirement, sf)
